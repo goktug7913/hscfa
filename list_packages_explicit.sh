63a06f8d9183cadaea84a7ffafd53523c5d69f16
G. Y. <goktug7913@gmail.com>
2023-11-29 07:49:34 -0300

    add list script

---
+++ list_packages_explicit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# List all explicitly installed packages, excluding base and base-devel
+pacman -Qei | awk '/^Name/ { name=$3 } /^Groups/ { if ( $3 != "base" && $3 != "base-devel" ) { print name } }'
